d20d96673de83d051cb654e23286301e901971a1
marie charlotte <charlottedoulcet90@gmail.com>
2026-03-20 13:46:59 +0100

    feat(dto): add QuoteListResponse and StatusUpdateRequest fields

--- src/main/java/com/flowmatic/flowmatic_back/dto/request/StatusUpdateRequest.java
+++ src/main/java/com/flowmatic/flowmatic_back/dto/request/StatusUpdateRequest.java
@@ -1,5 +1,16 @@
 package com.flowmatic.flowmatic_back.dto.request;
 
+import com.flowmatic.flowmatic_back.entity.enums.QuoteStatus;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class StatusUpdateRequest {
+
+  @NotNull(message = "Le statu est obligatoire")
+  private QuoteStatus status;
 
 }
