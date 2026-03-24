2ebe5d117e4bd134f225b1e9231290bcaaaffdb6
marie charlotte <charlottedoulcet90@gmail.com>
2026-03-24 20:06:55 +0100

    feat(agency): add AgencyResponse DTO and AgencyMapper

--- src/main/java/com/flowmatic/flowmatic_back/dto/response/agency/AgencyResponse.java
+++ src/main/java/com/flowmatic/flowmatic_back/dto/response/agency/AgencyResponse.java
@@ -1,5 +1,19 @@
 package com.flowmatic.flowmatic_back.dto.response.agency;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AgencyResponse {
+
+  private Long id;
+  private String name;
+  private String logoUrl;
+  private String primaryColor;
+  private String secondaryColor;
+  private String termsAndConditions;
 
 }
