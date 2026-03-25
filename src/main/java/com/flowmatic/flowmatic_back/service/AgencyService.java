fc428102c87cfbeee74c1d731dca2dbd6699e47b
marie charlotte <charlottedoulcet90@gmail.com>
2026-03-25 11:40:31 +0100

    feat(employees): add findByIdAndAgencyId to UserRepository

--- src/main/java/com/flowmatic/flowmatic_back/service/AgencyService.java
+++ src/main/java/com/flowmatic/flowmatic_back/service/AgencyService.java
@@ -2,7 +2,6 @@ package com.flowmatic.flowmatic_back.service;
 
 import com.flowmatic.flowmatic_back.dto.request.agency.AgencyUpdateRequest;
 import com.flowmatic.flowmatic_back.dto.response.agency.AgencyResponse;
-import com.flowmatic.flowmatic_back.repository.AgencyRepository;
 
 public interface AgencyService {
 
